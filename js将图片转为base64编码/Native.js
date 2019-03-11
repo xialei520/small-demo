71611678cd707f86707204ad2526adaebd690e93
xialei <377284071@qq.com>
2019-03-11 21:43:30 +0800

    pdf

--- js将图片转为base64编码/Native.js
+++ js将图片转为base64编码/Native.js
@@ -1,12 +1,3 @@
-export default {
-	getImageBase64(img, ext){
-		var canvas = document.createElement()('canvas');
-		canvas.width = img.width;
-		canvas.height = img.height;
-		var ctx = canvas.getContext('2d');
-		ctx.drawImage(img, 0, 0, img.width, img.height);
-		var dataUrl = canvas.toDataURL('image/'+ ext);
-		canvas = null;
-		return dataUrl;
-	}
-}
+ 
+	
+ 
