bacbc94b79e3f80d2686f3236a96050d84a657a6
xialei <377284071@qq.com>
2019-07-26 20:30:48 +0800

    7.26

--- npm包/rimraf.js
+++ npm包/rimraf.js
@@ -1,6 +1,6 @@
 const rm = require('rimraf');  //rimraf 包的作用：以包的形式包装rm -rf命令，用来删除文件和文件夹的，不管文件夹是否为空，都可删除.
 const path = require('path');
-let addr = path.resolve(__dirname, 'node_modules')
+let addr = path.resolve(__dirname, 'example.zip')
 console.log(addr)
 rm(addr, function(err) {
 	console.log(err);
