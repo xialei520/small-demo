6fbbfc0a87761a6a4ac0504f93764b648fff6cbc
xialei <377284071@qq.com>
2020-05-05 10:44:18 +0800

    5.5webpack-手势密码

--- WEBPACK4.0version/src/index.js
+++ WEBPACK4.0version/src/index.js
@@ -4,6 +4,16 @@ console.log(a);
 import './index.css';
 import './style.less';
 //只要组件有更新就刷新
-if(module.hot){
+if (module.hot) {
     module.hot.accept();
 }
+
+import Vue from 'vue';
+
+import Entry from './index.vue';
+
+let vm = new Vue({
+
+    el: "#app",
+    render: h => h(Entry)
+});
---
+++ webpack-cli/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="app">
+        <div class="title">
+            <p @click="jump(1)">手势登陆</p>
+            <p @click="jump(2)">其他</p>
+            <p @click="jump(3)">其他</p>
+        </div>
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    methods: {
+        jump(res) {
+            if (res === 1 || res === 2 || res === 3) {
+                this.$router.push({ name: "gesture" });
+            } else {
+                this.$router.push({ name: "gesture" });
+            }
+        }
+    }
+};
+</script>
+
+<style lang="css">
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.title {
+    width: 100%;
+    display: flex;
+}
+.title p {
+    flex: 1;
+}
+</style>
