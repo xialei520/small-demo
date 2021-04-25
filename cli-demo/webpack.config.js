20c8f2e93b69083eecd4424982e376d55530d5c2
xialei <377284071@qq.com>
2021-04-25 22:09:56 +0800

    dd

--- cli-demo/webpack.config.js
+++ cli-demo/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const px2vwLoader = path.resolve(__dirname, 'src/loaders/px2vw-loader.js');
 module.exports = {
+    mode: 'development',
     entry:   './src/start.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -15,10 +17,18 @@ module.exports = {
     },
     devtool: 'source-map',
     devServer: {
+        host: '127.0.0.1',
         contentBase: path.resolve(__dirname, 'dist'),
         compress: true,
         port: 8888,
-        open: true
+        open: false
+        },
+        //自定义loader
+        resolveLoader: {
+                //设置别名
+                alias: {
+                    'px2vw-loader': px2vwLoader
+                },
     },
     module: {
         rules: [
@@ -28,7 +38,15 @@ module.exports = {
             },
             {
                 test: /\.less$/,
-                use: ['style-loader', 'css-loader', 'less-loader']
+                use: ['style-loader', 'css-loader',
+                    'less-loader',
+                    {
+                        loader: 'px2vw-loader',
+                        options: {
+                            name: 'xialei'
+                        }
+                    }
+                ]
             }
         ],
         
