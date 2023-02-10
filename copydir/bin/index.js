2736bda85679769397c5b44004b897fab2290775
xialei <377284071@qq.com>
2023-02-10 22:06:30 +0800

    copydir全局npm包

---
+++ copydir/bin/index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import path from "path";
+import { readFileSync, access, constants } from "fs";
+import { exec, execSync } from "child_process";
+
+//package.json的路径
+let pkgPath = path.resolve(process.cwd(), "./package.json");
+//取出项目名称
+let basename = path.basename(process.cwd());
+//取出项目版本号
+let { version } = JSON.parse(readFileSync(pkgPath, "utf8"));
+
+if (process.argv.length < 4) {
+    console.log("参数有误，请重新输入！", process.argv);
+    process.exit(0);
+}
+
+let src = process.argv[2];
+let dest = path.resolve(process.argv[3], `./v${version}`);
+
+access(dest, constants.F_OK, async (err) => {
+    if (err) {
+        await execSync(`cd ${process.argv[3]} && mkdir v${version}`);
+    }
+    exec(`XCOPY ${src} ${dest} /S /E /Y`, { stdio: "inherit" }, async (err, stdout, stderr) => {
+        if (err) throw err;
+        console.log("copy successfully !!!");
+    });
+});
--- npm包/archiver.js
+++ npm包/archiver.js
@@ -1,45 +1,36 @@
 //archiver是一个在nodejs中能跨平台实现打包功能的模块，可以打zip和tar包，是一个比较好用的三方模块。
-const archiver = require('archiver');
-const fs = require('fs');
-const path = require('path');
+const archiver = require("archiver");
+const fs = require("fs");
+const path = require("path");
 
 //创建输出流
-const output = fs.createWriteStream(__dirname + '/example.zip');
+const output = fs.createWriteStream(__dirname + "/example.zip");
 
 //生成archiver对象，打包类型为zip
-let archive = archiver('zip', {
-<<<<<<< HEAD
-  zlib: { level: 9 }, //压缩等级
-  comment: '123456789',
-  forceZip64: true
-=======
-  zlib: { level: 9 } //压缩等级
->>>>>>> a356f6281b64bfd772e3ac22365d52cc9a5e7648
-})
-
-
-output.on('close', function () {
-
-  let size = archive.pointer() / 1000;
-  size = size > 1000 ? (size / 1000).toFixed(2) + 'MB' : parseInt(size);
-  console.log('example.zip: ' + size)
-
+let archive = archiver("zip", {
+    zlib: { level: 9 }, //压缩等级
+    comment: "123456789",
+    forceZip64: true
 });
 
-output.on('end', function () {
-  console.log('Data has been drained');
+output.on("close", function () {
+    let size = archive.pointer() / 1000;
+    size = size > 1000 ? (size / 1000).toFixed(2) + "MB" : parseInt(size);
+    console.log("example.zip: " + size);
 });
 
+output.on("end", function () {
+    console.log("Data has been drained");
+});
 
-
-archive.on('error', function (err) {
-  throw err;
+archive.on("error", function (err) {
+    throw err;
 });
 
 //将打包对象与输出流关联
 archive.pipe(output);
 
-const h5FilePath = path.resolve(__dirname, 'node_modules');
+const h5FilePath = path.resolve(__dirname, "node_modules");
 var file1 = h5FilePath;
 //将被打包文件的流添加进archiver对象中
 // archive.append(fs.createReadStream(file1), { name: 'app.js' });
